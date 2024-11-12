e899753d39acf213fbd9deb47c47863cce259209
superstudentcyberswir <01195923@pw.edu.pl>
2024-11-13 00:37:34 +0100

    Zadanie 2 - Implementacja poprawki oraz testu jednostkowego weryfikującego walidator.

--- Java/spring-thymeleaf-crud-example/src/main/java/com/example/thymeleaf/dto/CreateStudentDTO.java
+++ Java/spring-thymeleaf-crud-example/src/main/java/com/example/thymeleaf/dto/CreateStudentDTO.java
@@ -4,9 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Getter
@@ -14,10 +12,12 @@ import java.time.LocalDate;
 public class CreateStudentDTO {
 
     @NotEmpty(message = "{NotEmpty.name}")
+    @Size(min = 3, max = 50, message = "{Size.name}")
     private String name;
 
     @Email
     @NotEmpty(message = "{NotEmpty.email}")
+    @Size(max = 100, message = "{Size.email}")
     private String email;
 
     @NotNull(message = "{NotNull.birthday}")
@@ -25,23 +25,37 @@ public class CreateStudentDTO {
     private LocalDate birthday;
 
     @NotEmpty(message = "{NotEmpty.zipCode}")
+    @Size(min = 5, max = 10, message = "{Size.zipCode}")
+    @Pattern(regexp = "^[0-9]{5}(-?[0-9]{3})?$", message = "{Pattern.zipCode}")
     private String zipCode;
 
     @NotEmpty(message = "{NotEmpty.street}")
+    @Size(min = 2, max = 100, message = "{Size.street}")
+    @Pattern(regexp = "^[a-zA-Z0-9\\s.-]$", message = "{Pattern.street}")
     private String street;
 
     @NotEmpty(message = "{NotEmpty.number}")
+    @Size(min = 1, max = 10, message = "{Size.number}")
+    @Pattern(regexp = "^[a-zA-Z0-9\\s./-]$")
     private String number;
 
+    @Size(max = 50, message = "{Size.complement")
+    @Pattern(regexp = "^[a-zA-Z0-9\\s./-]$", message = "{Pattern.complement}")
     private String complement;
 
     @NotEmpty(message = "{NotEmpty.district}")
+    @Size(min = 2, max = 20, message = "{Size.district}")
+    @Pattern(regexp = "^[a-zA-Z\\s.-]$", message = "{Pattern.district}")
     private String district;
 
     @NotEmpty(message = "{NotEmpty.city}")
+    @Size(min = 2, max = 50, message = "{Size.city}")
+    @Pattern(regexp = "^[a-zA-Z\\s.-]$", message = "{Pattern.city}")
     private String city;
 
     @NotEmpty(message = "{NotEmpty.state}")
+    @Size(min = 2, max = 20, message = "{Size.state}")
+    @Pattern(regexp = "^[a-zA-Z\\s.-]$", message = "{Pattern.state}")
     private String state;
 
 }
--- Java/spring-thymeleaf-crud-example/src/test/java/com/example/thymeleaf/SpringThymeleafTemplateApplicationTests.java
+++ Java/spring-thymeleaf-crud-example/src/test/java/com/example/thymeleaf/SpringThymeleafTemplateApplicationTests.java
@@ -1,13 +1,72 @@
 package com.example.thymeleaf;
 
+import com.example.thymeleaf.dto.CreateStudentDTO;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.time.LocalDate;
+import java.util.Set;
+
+import static org.hibernate.validator.internal.util.Contracts.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 @SpringBootTest
 class SpringThymeleafTemplateApplicationTests {
+    private Validator validator;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
+
+    @BeforeEach
+    void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    void incorrectInputTest_XSS() {
+        CreateStudentDTO incorrect_student = new CreateStudentDTO();
+        incorrect_student.setName("<script>alert(1)</script>");
+        incorrect_student.setEmail("test@test.com");
+        incorrect_student.setBirthday(LocalDate.now());
+        incorrect_student.setZipCode("12345");
+        incorrect_student.setStreet("street");
+        incorrect_student.setNumber("1/23");
+        incorrect_student.setDistrict("district");
+        incorrect_student.setCity("city");
+        incorrect_student.setState("state");
+
+        validator.validate(incorrect_student);
+
+        Set<ConstraintViolation<CreateStudentDTO>> violations = validator.validate(incorrect_student);
+
+        assertFalse(violations.isEmpty(), "Validation should fail because name contains XSS content.");
+    }
+
+    @Test
+    void correctInputTest() {
+        CreateStudentDTO correct_student = new CreateStudentDTO();
+        correct_student.setName("Test test");
+        correct_student.setEmail("test@test.com");
+        correct_student.setBirthday(LocalDate.now());
+        correct_student.setZipCode("12345");
+        correct_student.setStreet("street");
+        correct_student.setNumber("1/23");
+        correct_student.setDistrict("district");
+        correct_student.setCity("city");
+        correct_student.setState("state");
+
+        validator.validate(correct_student);
+
+        Set<ConstraintViolation<CreateStudentDTO>> violations = validator.validate(correct_student);
+
+        assertTrue(violations.isEmpty(), "Validation should pass when all values are valid.");
+    }
 
 }
